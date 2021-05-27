e704a93ac7f1c331b62247789adeec6470c58b82
Version-2 <84303889+Version-2@users.noreply.github.com>
2021-05-27 15:42:50 +0530

    Update settings.py

--- Vendorsinprojectversion2/settings.py
+++ Vendorsinprojectversion2/settings.py
@@ -160,10 +160,10 @@ REST_FRAMEWORK = {
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-STATICFILES_DIRS=[
-    os.path.join(BASE_DIR,'static')
-]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+# STATICFILES_DIRS=[
+#     os.path.join(BASE_DIR,'static')
+# ]
 # MEDIA_URL='/media/'
 # MEDIA_ROOT=os.path.join(BASE_DIR,'mediafiles')
 # MEDIAFILES_DIRS = os.path.join(BASE_DIR, "media")

1639f180be32a63b53e0eb8066653996bdd412a9
iamharishshetty <66236853+iamharishshetty@users.noreply.github.com>
2021-07-01 10:17:55 +0530

    removed pagination

--- Vendorsinprojectversion2/settings.py
+++ Vendorsinprojectversion2/settings.py
@@ -149,8 +149,8 @@ REST_FRAMEWORK = {
         'rest_framework.parsers.FormParser',
         'rest_framework.parsers.MultiPartParser',
     ],
-   'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 20
+   # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+   #  'PAGE_SIZE': 20
 }
 
 

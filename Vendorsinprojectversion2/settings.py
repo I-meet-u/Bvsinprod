03bc0c6fe673caa7eea87c3269623438f780cd2c
iamharishshetty <66236853+iamharishshetty@users.noreply.github.com>
2021-07-06 20:35:06 +0530

    vm file

--- Vendorsinprojectversion2/settings.py
+++ Vendorsinprojectversion2/settings.py
@@ -13,9 +13,7 @@ import os.path
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-
-
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path("D:\\videos call").resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -159,7 +157,6 @@ REST_FRAMEWORK = {
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 

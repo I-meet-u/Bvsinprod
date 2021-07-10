5d95b28ae9d68c2db6495d0f5cdb43ac2ac5359a
Vyshnavi171 <66592894+Vyshnavi171@users.noreply.github.com>
2021-07-10 19:13:39 +0530

    rfq_category

--- BiddingApp/views.py
+++ BiddingApp/views.py
@@ -54,7 +54,7 @@ class BuyerProductBiddingView(viewsets.ModelViewSet):
 
             return super().create(request, *args, **kwargs)
         else:
-            return Response({'status':204,'message':'Rfq Code Settings Not Present'},status=204)
+            return Response({'status':204,'message':'Rfq Code Settings Not Present,Please Create Rfq in Settings'},status=204)
 
 
 class BiddingBuyerProductDetailsView(viewsets.ModelViewSet):

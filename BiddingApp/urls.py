dbd597cea032f74de2134237e29fed7d2f3acfe5
iamharishshetty <66236853+iamharishshetty@users.noreply.github.com>
2021-07-15 17:35:20 +0530

    file

--- BiddingApp/urls.py
+++ BiddingApp/urls.py
@@ -8,7 +8,7 @@ router.register('buyer-product-bidding',views.BuyerProductBiddingView)
 router.register('buyer-product-details',views.BiddingBuyerProductDetailsView)
 router.register('rfq-code-settings',views.RfqCodeSettingsView)
 router.register('rfq-terms-description',views.RfqTermsDescriptionView)
-
+# -------
 urlpatterns = [
     path('bidding-router-urls/',include(router.urls)),
     path('get-buyer-product-details/',views.get_buyer_product_based_on_userid_pk),

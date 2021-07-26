facc3f96593a3a1c8c12b0534d22293a64ef0a85
Vyshnavi171 <66592894+Vyshnavi171@users.noreply.github.com>
2021-07-26 17:14:36 +0530

    new_commit

--- Vendorsinprojectversion2/settings.py
+++ Vendorsinprojectversion2/settings.py
@@ -158,8 +158,16 @@ REST_FRAMEWORK = {
 
 # STATIC_URL = '/static/'
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = 'media/'
+
+
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# MEDIA_URL = 'media/'
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "mediafiles")
+MEDIAFILES_DIRS = (os.path.join(BASE_DIR, "media"),)
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 

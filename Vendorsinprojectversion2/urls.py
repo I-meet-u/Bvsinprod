c3ecac0deb23bca9fea3fb78dd0d351519816bc2
Vyshnavi171 <66592894+Vyshnavi171@users.noreply.github.com>
2021-07-26 17:08:10 +0530

    new_commit

--- Vendorsinprojectversion2/urls.py
+++ Vendorsinprojectversion2/urls.py
@@ -31,4 +31,4 @@ urlpatterns = [
     path('admin-page/',include('AdminApp.urls'))
 ]
 if settings.DEBUG:
-    static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)

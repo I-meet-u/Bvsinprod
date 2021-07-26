6843cfddb14c34e1a4dd5e021b2147fefb20da69
Vyshnavi171 <66592894+Vyshnavi171@users.noreply.github.com>
2021-07-26 16:41:20 +0530

    new_commit

--- Vendorsinprojectversion2/settings.py
+++ Vendorsinprojectversion2/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
     'rest_framework',
     'corsheaders',
     'RegistrationApp',
@@ -157,11 +156,11 @@ REST_FRAMEWORK = {
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'assests/')
-STATICFILES_DIRS = [
-        os.path.join(BASE_DIR, 'static'),
-]
+# STATIC_URL = '/static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'assests/')
+# STATICFILES_DIRS = [
+#         os.path.join(BASE_DIR, 'static'),
+# ]
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = 'media/'
 # Default primary key field type

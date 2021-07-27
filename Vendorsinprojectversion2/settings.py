97acdd90f1ae3abe9867bf08b0de13ed024c89b0
Vyshnavi171 <66592894+Vyshnavi171@users.noreply.github.com>
2021-07-27 11:44:26 +0530

    new_commit

--- Vendorsinprojectversion2/settings.py
+++ Vendorsinprojectversion2/settings.py
@@ -89,7 +89,7 @@ WSGI_APPLICATION = 'Vendorsinprojectversion2.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'NewProductionDB',
+        'NAME': 'NewTestDB',
         'USER': 'vendorsincommerce@vendorsincommerce',
         'PASSWORD':'Vendorsin@vsin2021',
         'HOST': 'vendorsincommerce.postgres.database.azure.com',

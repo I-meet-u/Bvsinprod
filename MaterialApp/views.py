89555aa04cdc23d9ecad5ec3a336bd4da3fb3a38
Vyshnavi171 <66592894+Vyshnavi171@users.noreply.github.com>
2021-09-09 16:06:24 +0530

    landing_page_bid

--- MaterialApp/views.py
+++ MaterialApp/views.py
@@ -1833,10 +1833,15 @@ def update_landing_page_status_to_decline(request):
 def get_landing_page_bidding_by_pk(request):
     data=request.data
     landingpk=data['landingpk']
+    basicarray={}
     try:
         landingobj=LandingPageBidding.objects.filter(id=landingpk).values()
         if len(landingobj)>0:
+            basicobj = BasicCompanyDetails.objects.get(updated_by_id=landingobj[0].get('updated_by_id'))
             vendorproductobj=VendorProduct_BasicDetails.objects.filter(vendor_product_id=landingobj[0].get('vendor_product_pk')).values()
+            basicarray.setdefault('company_code',str(basicobj.company_code))
+            basicarray.setdefault('company_name',basicobj.company_name)
+            landingobj[0].setdefault('basic_details',basicarray)
             return Response({'status':200,'message':'Landing Page Bidding List','landingpagedata':landingobj,'vendorproductdata':vendorproductobj},status=200)
         else:
             return Response({'status':204,'message':'Not Present','data':landingobj},status=200)

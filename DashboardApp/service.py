1087bff6e10ad2f99f4370bd6c36f485998b36b5
Vyshnavi171 <66592894+Vyshnavi171@users.noreply.github.com>
2021-09-18 16:37:14 +0530

    dashboard_count

--- DashboardApp/service.py
+++ DashboardApp/service.py
@@ -45,41 +45,46 @@ def get_deadline_date(userid,from_registration,auth_token):
         if r.status_code == 500:
             return json3
     except Exception as e:
-        return  e
-    # return deadline_date
+        return e
 
 
 def get_vendor_award_list(userid,auth_token):
-    auth={'Authorization':auth_token}
     json1 = {'status': 500, 'message': 'Not Present', 'data': []}
-    # url = "http://127.0.0.1:8000/bidding/awards-vendor-list/"
-    url="https://v2apis.vendorsin.com/bidding/awards-vendor-list/"
-    dataobj={'userid':userid}
-    r = requests.post(url, headers=auth, json=dataobj)
-    # vendoraward=r.json()
-    if r.status_code == 200:
-        vendoraward = r.json()
-        return vendoraward
-    if r.status_code == 500:
-        return json1
+    try:
+        auth={'Authorization':auth_token}
+        # url = "http://127.0.0.1:8000/bidding/awards-vendor-list/"
+        url="https://v2apis.vendorsin.com/bidding/awards-vendor-list/"
+        dataobj={'userid':userid}
+        r = requests.post(url, headers=auth, json=dataobj)
+        # vendoraward=r.json()
+        if r.status_code == 200:
+            vendoraward = r.json()
+            return vendoraward
+        if r.status_code == 500:
+            return json1
+    except Exception as e:
+        return e
 
 def get_purchase_order_vendor_list(userid,auth_token):
-    auth = {'Authorization': auth_token}
     json1 = {'status': 202, 'message': 'Not Present', 'data': []}
     json2 = {'status': 500, 'message': 'Not Present', 'data': []}
-    # url = "http://127.0.0.1:8000/bidding/purchase-order-vendors-list/"
-    url="https://v2apis.vendorsin.com/bidding/purchase-order-vendors-list/"
-    dataobj = {'userid': userid}
-    # r = requests.post(url, data=dataobj, headers=auth)
-    r = requests.post(url, headers=auth, json=dataobj)
-    # vendoraward=r.json()
-    if r.status_code == 200:
-        vendoraward = r.json()
-        return vendoraward
-    if r.status_code == 202:
-        return json1
-    if r.status_code == 500:
-        return json2
+    try:
+        auth = {'Authorization': auth_token}
+        # url = "http://127.0.0.1:8000/bidding/purchase-order-vendors-list/"
+        url="https://v2apis.vendorsin.com/bidding/purchase-order-vendors-list/"
+        dataobj = {'userid': userid}
+        # r = requests.post(url, data=dataobj, headers=auth)
+        r = requests.post(url, headers=auth, json=dataobj)
+        # vendoraward=r.json()
+        if r.status_code == 200:
+            vendoraward = r.json()
+            return vendoraward
+        if r.status_code == 202:
+            return json1
+        if r.status_code == 500:
+            return json2
+    except Exception as e:
+        return e
 
 def get_source_created_items(userid,auth_token):
     json1 = {'status': 204, 'message': 'Not Present', 'data': []}
@@ -99,25 +104,13 @@ def get_source_created_items(userid,auth_token):
         if r.status_code == 500:
             return json2
     except Exception as e:
-        return  e
-def get_business_request_list(userid,auth_token):
-    try:
-        auth = {'Authorization': auth_token,
-                }
-        url="https://v2apis.vendorsin.com/dashboard-page/sendergetbuzrequestdata/"
-        data = {'userid': userid}
-        r = requests.post(url, data=data, headers=auth)
-        print('-------------------------------------------------------',r)
-        return r.json()
-    except Exception as e:
-        print('error got',e)
+        return e
 
 def get_business_accept_list(userid,auth_token):
     json1 = {'status': 202, 'message': 'Not Present', 'data': []}
     json2 = {'status': 204, 'message': 'Not Present', 'data': []}
     json3 = {'status': 500, 'message': 'Not Present', 'data': []}
     try:
-        # url = "http://127.0.0.1:8000/dashboard-page/sendergetbuzrequestdata/"
         auth = {'Authorization': auth_token,
                 "Content-Type": "application/json"
                 }
@@ -134,7 +127,7 @@ def get_business_accept_list(userid,auth_token):
         if r.status_code == 500:
             return json3
     except Exception as e:
-        print('error got',e)
+        return e
 
 def get_business_connections(userid,auth_token):
     json1 = {'status': 202, 'message': 'Not Present', 'data': []}
@@ -157,13 +150,12 @@ def get_business_connections(userid,auth_token):
         if r.status_code == 500:
             return json3
     except Exception as e:
-        print('error got', e)
+        return e
 
 def get_business_requests_list(userid,auth_token):
     json2 = {'status': 204, 'message': 'Not Present', 'data': []}
     json3 = {'status': 500, 'message': 'Not Present', 'data': []}
     try:
-        # url = "http://127.0.0.1:8000/dashboard-page/sendergetbuzrequestdata/"
         auth = {'Authorization': auth_token,
                 "Content-Type": "application/json"
                 }
@@ -178,35 +170,23 @@ def get_business_requests_list(userid,auth_token):
         if r.status_code == 500:
             return json3
     except Exception as e:
-        print('error got',e)
+        return e
 
 def total_all_responses_buyer(userid,from_registration,auth_token):
+    json3 = {'status': 500, 'message': 'Not Present', 'data': []}
     try:
         auth = {'Authorization': auth_token}
         url="https://v2apis.vendorsin.com/bidding/bidding-data-response-count/"
         data={'userid':userid,'from_registration':from_registration}
-        r = requests.post(url,data=data,headers=auth)
-        totacount=r.json()
-        return  totacount
-
-
+        r = requests.post(url, headers=auth, json=data)
+        if r.status_code == 200:
+            vendoraward = r.json()
+            return vendoraward
+        if r.status_code == 500:
+            return json3
     except Exception as e:
         print('error response',e)
 
-
-
-def awarded(userid, auth_token):
-    try:
-        auth = {'Authorization': auth_token,
-                "Content-Type": "application/json"
-                }
-        url = "https://v2apis.vendorsin.com/bidding/get-all-types-of-awards/"
-        data = {'userid': userid}
-        r = requests.post(url, headers=auth, json=json.dumps(data)).json()
-        awarded = r
-        return awarded
-    except Exception as e:
-        print('error-------------------------------------', e)
 
 def get_vendor_published_list(userid,auth_token):
     auth={'Authorization':auth_token}

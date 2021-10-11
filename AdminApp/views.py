ea2a0b1ecbe611b9d13730fe6aceb7e6f3644b08
Vyshnavi171 <66592894+Vyshnavi171@users.noreply.github.com>
2021-10-11 15:03:09 +0530

    vendor_open_leads

--- AdminApp/views.py
+++ AdminApp/views.py
@@ -1683,26 +1683,42 @@ def fetch_buyer_product_details_admin(request):
         return Response({'status': 500, 'message': str(e)}, status=500)
 
 
+# @api_view(['post'])
+# @permission_classes((AllowAny,))
+# def fetch_vendor_open_leads(request):
+#     data=request.data
+#     rfqnumber=data['rfqnumber']
+#     try:
+#         if data['token'] == "4aoedpde123Vyeyweuo2":
+#             openleadsvendorobj=OpenLeadsVendorPublishRfq.objects.filter(vendor_rfq_number=rfqnumber).values().order_by('id')
+#             if len(openleadsvendorobj)>0:
+#                 for i in range(0, len(openleadsvendorobj)):
+#
+#
+#                     vendorterms = OpenLeadsVendorPublishTermsDescription.objects.filter(vendor_rfq_number=rfqnumber).values().order_by('id')
+#                     for k in range(0, len(vendorterms)):
+#                         print('correct')
+#                     openleadsvendorobj[i].__setitem__('product', vendorproductobj)
+#                     openleadsvendorobj[i].__setitem__('vendor_rfq_terms', vendorterms)
+#
+#                 return Response({'status': 200, 'message': 'Admin Added Products List', 'data': openleadsvendorobj}, status=200)
+#             else:
+#                 return Response({'status': 204, 'message': 'Not Present'}, status=204)
+#         else:
+#             return Response({'status': 400, 'message': 'Bad Request'}, status=400)
+#
+#     except Exception as e:
+#         return Response({'status': 500, 'message': str(e)}, status=500)
+
+
 @api_view(['post'])
 @permission_classes((AllowAny,))
-def fetch_vendor_open_leads(request):
-    data=request.data
-    rfqnumber=data['rfqnumber']
+def fetch_all_open_leads_rfq(request):
     try:
-        if data['token'] == "4aoedpde123Vyeyweuo2":
-            openleadsvendorobj=OpenLeadsVendorPublishRfq.objects.filter(vendor_rfq_number=rfqnumber).values().order_by('id')
-            if len(openleadsvendorobj)>0:
-                for i in range(0, len(openleadsvendorobj)):
-                    vendorproductobj = OpenLeadsVendorPublishItems.objects.filter(vendor_open_leads_pk=openleadsvendorobj[i].get('id')).values().order_by('id')
-                    for j in range(0, len(vendorproductobj)):
-                        print('s print')
-                    vendorterms = OpenLeadsVendorPublishTermsDescription.objects.filter(vendor_rfq_number=rfqnumber).values().order_by('id')
-                    for k in range(0, len(vendorterms)):
-                        print('correct')
-                    openleadsvendorobj[i].__setitem__('product', vendorproductobj)
-                    openleadsvendorobj[i].__setitem__('vendor_rfq_terms', vendorterms)
-
-                return Response({'status': 200, 'message': 'Admin Added Products List', 'data': openleadsvendorobj}, status=200)
+        if request.data['key']=="vsinadmindb":
+            openleadsobj=OpenLeadsRfq.objects.filter().values().order_by('id')
+            if len(openleadsobj)>0:
+                return Response({'status': 200, 'message': 'Open Leads Rfq List','data':openleadsobj},status=200)
             else:
                 return Response({'status': 204, 'message': 'Not Present'}, status=204)
         else:
@@ -1714,12 +1730,42 @@ def fetch_vendor_open_leads(request):
 
 @api_view(['post'])
 @permission_classes((AllowAny,))
-def fetch_all_open_leads_rfq(request):
+def fetch_vendor_open_leads(request):
+    data=request.data
+    rfqnumber=data['rfqnumber']
+    openleadsarray=[]
+    count=0
     try:
-        if request.data['key']=="vsinadmindb":
-            openleadsobj=OpenLeadsRfq.objects.filter().values().order_by('id')
-            if len(openleadsobj)>0:
-                return Response({'status': 200, 'message': 'Open Leads Rfq List','data':openleadsobj},status=200)
+        if data['token'] == "4aoedpde123Vyeyweuo2":
+            openleadsvendorobj=OpenLeadsVendorPublishRfq.objects.filter(vendor_rfq_number=rfqnumber).values().order_by('id')
+            if len(openleadsvendorobj)>0:
+                for i in range(0, len(openleadsvendorobj)):
+                    basicobj=BasicCompanyDetails.objects.filter(updated_by_id=openleadsvendorobj[i].get('updated_by_id')).values()
+                    vendorproductobj = OpenLeadsVendorPublishItems.objects.filter(vendor_open_leads_pk=openleadsvendorobj[i].get('id')).values().order_by('id')
+                    if len(vendorproductobj)>0:
+                        for j in range(0, len(vendorproductobj)):
+                            print('s print')
+                            if vendorproductobj[j].get('buyer_quantity') != "":
+                                count = count + int(vendorproductobj[j].get('buyer_quantity'))
+                            print('count-------------------------------', count)
+
+                    else:
+                        print('items not present')
+
+                    openleadsarray.append({'company_name': basicobj[0].get('company_name'),
+                                           'company_code': basicobj[0].get('company_code'),
+                                           'vendor_rfq_number': openleadsvendorobj[i].get('vendor_rfq_number'),
+                                           'final_amount': openleadsvendorobj[i].get('final_amount'),
+                                           'buyer_quantity':count,
+                                           'pk':openleadsvendorobj[i].get('id')
+
+                                           })
+                    count = 0
+
+
+
+
+                return Response({'status': 200, 'message': 'Admin Added Products List', 'data': openleadsarray}, status=200)
             else:
                 return Response({'status': 204, 'message': 'Not Present'}, status=204)
         else:

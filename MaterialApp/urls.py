fc0f14c9d4a835fa45bf3abf325677d9cc75228f
Vyshnavi171 <66592894+Vyshnavi171@users.noreply.github.com>
2021-10-18 11:39:05 +0530

    first_commit

--- MaterialApp/urls.py
+++ MaterialApp/urls.py
@@ -69,6 +69,7 @@ urlpatterns = [
     path('update-status-from-pending-to-reject/', views.update_status_from_pending_to_reject),
     path('update-status-from-pending-to-published/',views.update_status_from_pending_to_published),
     path('getawardlistoflistingleadsnew/',views.getawardlistoflistingleadsnew),
-    path('landing-page-listing-leads-rejected-list/',views.landing_page_listing_leads_rejected_list)
+    path('landing-page-listing-leads-rejected-list/',views.landing_page_listing_leads_rejected_list),
+    path('landing-page-published-list-by-user-id/',views.landing_page_published_list_by_user_id)
 
 ]

b7d5c18945b019dc0c69d7daff9a16ac1c1a42df
Vyshnavi171 <vyshnavi.ms@vendorsin.com>
2021-12-21 17:47:54 +0530

    contact_us

--- AdminApp/views.py
+++ AdminApp/views.py
@@ -2592,7 +2592,7 @@ def contact_us_send_mail(request):
 
                 api_instance = sib_api_v3_sdk.TransactionalEmailsApi(sib_api_v3_sdk.ApiClient(configuration))
                 subject = title
-                text_content = "Dear , " + company_name +"\n\n" +"Name : "+ name + "\n\n" +"Phone Number : " + phone+"\n\n" +"City :" +billobj[0].get('bill_city') + "\n\n" + 'Message : '+ message + "\n\n" + "Regards , "+"\n\n" + name + "\n\n" + "Note: Please Don't Share this email with anyone"
+                text_content = "Dear , " + company_name +"\n\n" + message + "\n\n" + "Regards , "+"\n" + name + "\n"+phone+ "\n" + billobj[0].get('bill_city')+"\n\n" + "Note: Please Don't Share this email with anyone"
                 sender = {"name": name, "email":email}
                 to = [{"email": regobj[0].get('username'), "name": regobj[0].get('contact_person')}]
                 send_smtp_email = sib_api_v3_sdk.SendSmtpEmail(to=to,text_content=text_content,

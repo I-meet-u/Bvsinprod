1cd634f44702c357dd89900ef8e4033810114732
Vyshnavi171 <vyshnavi.ms@vendorsin.com>
2022-02-22 10:30:25 +0530

    messages

--- MaterialApp/views.py
+++ MaterialApp/views.py
@@ -3524,6 +3524,30 @@ class BuyerProduct_RequirementsViewSet(viewsets.ModelViewSet):
     queryset = BuyerProduct_Requirements.objects.all()
     serializer_class = BuyerProduct_RequirementsSerializer
 
+
+    def create(self, request, *args, **kwargs):
+        values_array=request.data['values_array']
+        try:
+            for i in range(0,len(values_array)):
+                print(next(iter(values_array[i])))
+                buyerlabelvalue=next(iter(values_array[i]))
+                values = list(values_array[i].items())[0][1]
+
+
+                vendorobj=BuyerProduct_Requirements.objects.create(buyer_label_name=buyerlabelvalue,
+                                                                   buyer_default_value=values,
+                                                                   buyer_data_type=values_array[i].get('datatype'),
+                                                                   is_mandatory=values_array[i].get('mandatory'),
+                                                                   created_by=request.data['created_by'],
+                                                                   updated_by=SelfRegistration.objects.get(id=request.data['updated_by']),
+                                                                   vendor_product_basic_pk=VendorProduct_BasicDetails.objects.get(vendor_product_id=request.data['vendor_product_basic_pk']),
+                                                                   vendor_product_requirement_pk=VendorProduct_Requirements.objects.get(id=request.data['vendor_product_requirement_pk'])
+                                                                   )
+
+            return Response({'status':201,'message':'Buyer Product Requirements are  Created'},status=201)
+        except Exception as e:
+            return Response({'status':500,'error':str(e)},status=500)
+
     def get_queryset(self):
         buyerobj=BuyerProduct_Requirements.objects.filter(updated_by=self.request.GET.get('updated_by')).order_by('id')
         if buyerobj:

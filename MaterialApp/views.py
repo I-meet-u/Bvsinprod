a6aa5e41cdcac33d65b28546baebc001bc13aafb
Vyshnavi171 <vyshnavi.ms@vendorsin.com>
2022-02-23 17:08:39 +0530

    messages

--- MaterialApp/views.py
+++ MaterialApp/views.py
@@ -3078,8 +3078,11 @@ def get_vendor_product_details_difference_industry_category(request):
             vendorobjmaincore=VendorProduct_BasicDetails.objects.filter(company_code=ccode).distinct('core_sector').values()
             if len(vendorobjmaincore)>0:
                 for i in range(0,len(vendorobjmaincore)):
-                    maincoreobj=MaincoreMaster.objects.filter(maincore_name=vendorobjmaincore[i].get('core_sector')).values()
+                    print(vendorobjmaincore[i].get('core_sector'))
+                    maincoreobj=MaincoreMaster.objects.filter(maincore_name__icontains=vendorobjmaincore[i].get('core_sector')).values()
+                    print(len(maincoreobj))
                     if maincoreobj[0].get('maincore_image')==None or maincoreobj[0].get('maincore_image')=="":
+                        print('sss')
                         vendorobjmaincore[i].__setitem__('maincore_image_url',"")
                     else:
                         vendorobjmaincore[i].__setitem__('maincore_image_url', maincoreobj[0].get('maincore_image'))
@@ -3089,35 +3092,27 @@ def get_vendor_product_details_difference_industry_category(request):
             if len(vendorobjcategory) > 0:
                 for i in range(0, len(vendorobjcategory)):
                     categoryobj = CategoryMaster.objects.filter(
-                        category_name=vendorobjcategory[i].get('category')).values()
+                        category_name__icontains=vendorobjcategory[i].get('category')).values()
                     if categoryobj[0].get('category_image') == None or categoryobj[0].get('category_image') == "":
                         print('sss')
                         vendorobjcategory[i].__setitem__('category_image_url', "")
 
                     else:
                         vendorobjcategory[i].__setitem__('category_image_url', categoryobj[0].get('category_image'))
-
-
             vendorobjsubcategory = VendorProduct_BasicDetails.objects.filter(company_code=ccode).distinct(
                 'sub_category').values()
             if len(vendorobjsubcategory) > 0:
                 for i in range(0, len(vendorobjsubcategory)):
                     subcategoryobj = SubCategoryMaster.objects.filter(
-                        sub_category_name=vendorobjsubcategory[i].get('sub_category')).values()
+                        sub_category_name__icontains=vendorobjsubcategory[i].get('sub_category')).values()
                     if subcategoryobj[0].get('sub_category_image') ==None or subcategoryobj[0].get('sub_category_image') =="":
                         vendorobjsubcategory[i].__setitem__('sub_category_image_url',"")
                     else:
                         vendorobjsubcategory[i].__setitem__('sub_category_image_url',
                                                             subcategoryobj[0].get('sub_category_image'))
 
-            return Response(
-                {'status': 200, 'message': 'Vendor Product Basic Details List', 'maincore': vendorobjmaincore,
-                 'category': vendorobjcategory, 'Subcategory': vendorobjsubcategory},
-                status=status.HTTP_200_OK)
-
-            return Response(
-                {'status': 200, 'message': 'Vendor Product Basic Details List', 'maincore': vendorobjmaincore},
-                status=status.HTTP_200_OK)
+            return Response({'status': 200, 'message': 'Vendor Product Basic Details List', 'maincore': vendorobjmaincore,
+                 'category': vendorobjcategory, 'Subcategory': vendorobjsubcategory},status=status.HTTP_200_OK)
         else:
             return Response(
                 {'status': 401, 'message': 'UnAuthorized'},

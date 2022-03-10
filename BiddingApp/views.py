0e9ac86ac6785dc5801863599c0d959ff6c07ae5
Vyshnavi171 <vyshnavi.ms@vendorsin.com>
2022-03-10 22:05:06 +0530

    source

--- BiddingApp/views.py
+++ BiddingApp/views.py
@@ -5960,7 +5960,6 @@ def delete_terms_by_id(request):
         return Response({'status': 500, 'error': str(e)}, status=500)
 
 
-
 @api_view(['post'])
 def get_source_po_details_based_on_vendor_user_id(request):
     data=request.data
